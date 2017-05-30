6099c4f28b366d02d80028a82896d45588f9bce1
speedblizzz <speedblizzz@yandex.ru>
2017-05-30 22:17:20 +0500

    вывод иконок соц сетей в шапке

--- main.php
+++ main.php
@@ -10,6 +10,9 @@
   <p class="header-descr1 animated bounceIn ">Оформление свадьбы на ваш вкус</p>
   <p class="header-descr2 animated bounceIn ">Букеты на заказ</p>
   <p class="header-descr3 animated bounceIn ">Любые виды цветочного декора</p>
-    <span data-href="https://www.youtube.com/watch?v=uvG7h0wjaMg&feature=youtu.be" class="logo-play animated bounceIn youtuber "></span>
+    <a class="icon-head vk-icon-head animated bounceIn" href="https://vk.com/imflorist " target=_blank></a>
+    <a class="icon-head fb-icon-head animated bounceIn" href="https://www.facebook.com/profile.php?id=100010770979023" target=_blank></a>
+    <a class="icon-head inst-icon-head animated bounceIn" href="https://www.instagram.com/imflorist/" target=_blank"></a>
+    <span data-href="https://www.youtube.com/watch?v=uvG7h0wjaMg&feature=youtu.be" class="icon-head youtube-icon-head animated bounceIn youtuber "></span>
   <a class="go-feed animated bounceIn" href="">Связаться с нами</a>
 </div>
